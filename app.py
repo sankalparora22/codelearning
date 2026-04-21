1a63ec195e5413a77d3826a7a07a33bb1e685454
sankalparora22 <arora.sanjeev6@gmail.com>
2026-04-21 03:04:12 -1200

    Update app.py

--- app.py
+++ app.py
@@ -170,5 +170,7 @@ def logout():
     return redirect("/")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
